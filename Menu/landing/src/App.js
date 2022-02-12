85d874b2520f8438780e1816d147624b890d3364
Azay Genius <azay.genius8@gmail.com>
2022-02-12 18:24:30 +0000

    new App.js created

--- Menu/landing/src/App.js
+++ Menu/landing/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import './App.css';
+import Navbar from "./components/Navbar";
 function App() {
-    return (
-
-    )
+  return (
+    <div className="container">
+    
+        <Navbar/>
+    </div>
+  );
 }
 
 export default App;
