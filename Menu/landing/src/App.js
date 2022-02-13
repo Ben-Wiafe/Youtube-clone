9b7981cb145a50309a6733b2a9a1a3fbcb97beea
Azay Genius <azay.genius8@gmail.com>
2022-02-13 20:30:56 +0000

    Main js component

--- Menu/landing/src/App.js
+++ Menu/landing/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="container">
     
         <Navbar/>
+        <Main/>
+        <footer>
+        <Footer/>
+        </footer>
+        
     </div>
   );
 }
