c042314bf44a99cfd55ffab25de1555a92f54f96
Azay Genius <azay.genius8@gmail.com>
2022-02-13 20:32:33 +0000

    Navbar component

--- Menu/landing/src/components/Navbar.js
+++ Menu/landing/src/components/Navbar.js
@@ -3,8 +3,18 @@ import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <div className='container'>
-          <h3>Navbar</h3>
+    <div className='main'>
+      <header className='header_Nav'> 
+      <h3 className='logo_font'>Genius</h3>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Furniture</li>
+          <li>Blog</li>
+          <li>Contact Us</li>
+        </ul>
+        <span className='Login_button'>Login</span>
+      </header>
     </div>
   )
 }
