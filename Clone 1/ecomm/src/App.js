5971295f505fc00567f4fa1b9402b8d99618d887
Azay Genius <azay.genius8@gmail.com>
2022-02-15 04:29:44 +0000

    initial commit

--- Clone 1/ecomm/src/App.js
+++ Clone 1/ecomm/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
+import ScrollToTop from "./components/ScrollToTop";
+import Portfolio from "./components/Portfolio";
+import Services from "./components/Services";
+import Testimonial from "./components/Testimonial";
+import Product from "./components/Product";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <ScrollToTop/>
+     <Navbar/>
+     <Hero/>
+     <Services/>
+     <Portfolio/>
+     <Testimonial/>
+     <Product/>
+     <Newsletter/>
+     <Footer/>
     </div>
   );
 }
